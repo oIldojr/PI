9072b88c06e4c9cfc4e9cbaa6841a71b1b8cc7d7
Ildo <ildomoura90@gmail.com>
2025-06-27 08:24:32 -0300

    Models finalizado

--- models/clinica_model.py
+++ models/clinica_model.py
@@ -1,14 +1,37 @@
 from db import conectBD
+class Clinica:
+    def __init__(self,nome,cnpj,telefone,id = None):
+        self.id = id 
+        self.nome = nome
+        self.cnpj = cnpj
+        self.telefone = telefone
 
-def inserir_():
+    def salvar_clinica():
     conn = conectBD()
     cursor = conn.cursor()
 
-    sql = """
-    INSERT INTO medicos ()
-    VALUES ()
-    """
-    valores = ()
+       if self.id is None:
+
+            sql = """
+            INSERT INTO clinica (nome,cnpj,telefone)
+            VALUES (%s, %s, %s, %s)
+            """
+            valores = (self.nome, self.cnpj, self.telefone)
+
+        else:
+             sql = "UPDATE cnpj SET nome=%s, cnpj=%s,telefone=%s WHERE id=%s)"
+
+             valores = (self.nome, self.cnpj, self.telefone,self.id)
+
+    def deletar(self):
+        if self.id is not None:
+            conn = conectar()
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM clientes WHERE id = %s",(self.id))    
+            conn.commit()
+            cursor.close()
+            conn.close
+
     cursor.execute(sql, valores)
     conn.commit()
 
